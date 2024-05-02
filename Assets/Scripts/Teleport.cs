b14708e7b656f8f044cc2d657085b0af940e5279
Paulentius <pashpichug@mail.ru>
2024-05-02 20:58:57 +0600

    Added Dungeon location and Teleportation

--- Assets/Scripts/Teleport.cs
+++ Assets/Scripts/Teleport.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Teleport : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform teleportDestination; // Ссылка на точку, куда нужно телепортировать персонаж
+    
+    public float teleportDelay = 0.5f; // Задержка перед телепортацией
+
+    private bool teleporting = false;
+
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player") && !teleporting)
+        {
+            StartCoroutine(Tp(other.transform));
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator Tp(Transform playerTransform)
     {
+        teleporting = true;
+
+        // Ваш код анимации телепортации, если нужен
+
+        yield return new WaitForSeconds(teleportDelay);
+
+        // Телепортация персонажа
+        playerTransform.position = teleportDestination.position;
+
         
+
+        teleporting = false;
     }
 }
